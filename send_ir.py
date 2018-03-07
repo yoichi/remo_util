ad886032b2b758468839ccaaf5a45bbf69ec96f2
Yoichi Nakayama <yoichi.nakayama@gmail.com>
2018-03-08 06:29:33 +0900

    Dispatch all remaining arguments to the subprocess.

--- send_ir.py
+++ send_ir.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         description='send IR signal to Nature Remo via Local API')
     parser.add_argument('ip_address', help='IP address of Nature Remo')
     parser.add_argument('signal_format', help='IR signal format (aeha|sony)')
-    parser.add_argument('args', nargs='*')
+    parser.add_argument('args', nargs=argparse.REMAINDER)
     args = parser.parse_args()
     send_ir(ip_address=args.ip_address, signal_format=args.signal_format,
             rest_args=args.args)
